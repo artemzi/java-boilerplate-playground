9c37ebb02d0d937bad9836be9ac2693277a89a47
Artem Zinoviev <arzinoviev@gmail.com>
2018-09-13 18:20:16 +0300

    add todo

--- src/main/java/com/github/artemzi/Play.java
+++ src/main/java/com/github/artemzi/Play.java
@@ -9,10 +9,10 @@ import java.util.List;
 public class Play {
 
     public static void main(String[] args) {
-        List<Person> persons = PersonGenerator.fillWithValues(10);
+        List<Person> persons = PersonGenerator.fillWithValues(100);
         for (Person p : persons) {
             print(System.out, p.toString());
-        }
+        } // TODO: try to play with search / sort by different field. Storing object etc.
     }
 
     static void print(PrintStream out, String data) {
